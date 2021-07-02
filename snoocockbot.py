705d114d4ca6916ec2f27999cf8ca007968ba0ce
Deltara3 <50772027+Deltara3@users.noreply.github.com>
2021-07-02 13:11:09 -0400

    Add current bot code
    Note: Currently the bot replies with braille in the main code. This may be changed at a later date.

---
+++ snoocockbot.py
@@ -0,0 +1,44 @@
+# Please don't copy until the bot is suspended. Then I don't really care. (Reddit please no suspension)
+
+import praw
+import os
+from keep_alive import keep_alive
+from multiprocessing import Process
+
+# file variable name = os.environ['variable name']
+# Hopefully above makes sense. If not please suggest a change.
+
+reddit = praw.Reddit(
+  client_id = bot_client_id, # Recommended to use a hidden variable.
+  client_secret = bot_client_id, # Recommended to use a hidden variable.
+  user_agent = "follow Reddit guidelines when filling this in", # Please do
+  username = "obviously put your bot name here",
+  password = bot_password, # Recommended to use a hidden variable.
+  validate_on_submit = True
+)
+
+# For logging purposes
+print(reddit.user.me())
+
+# haha funny function name
+def lmaoSnooCockBot():
+  for item in reddit.inbox.stream(skip_existing=True):
+    if "u/your bot name, pretty easy to understand" in item.body.lower():
+      # For logging purposes
+      print(20*"--")
+      print(item.body)
+      print("by: ", item.author)
+      print(20*"--")
+      try:
+        item.reply('    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡏⠀⠀⠈⡆⠀⣠⠞⢻⡀⠀⠀⠀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢷⣶⣤⠠⠓⠋⠁⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⣀⣀⡀⠀⢀⣾⠃⠀⠀⠀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡤⠖⠛⠉⠉⠉⠉⠉⠙⠛⢀⣀⣀⡀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⢀⡾⡽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠉⠳⡄⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠸⣿⢡⣾⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⠀⠀\n    ⠀⠀⠀⠀⠀⠀⢰⠇⠘⠛⠃⠀⠀⠀⠀⠒⠒⠢⠀⠀⠀⠠⡤⢄⡼⠃⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠸⡇⠀⠀⢀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠘⢻⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⠻⣄⠀⠀⠈⠉⠉⠉⠀⠀⣴⠛⣦⠀⠀⢀⠞⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣤⣀⡀⠀⠀⠀⠀⡸⠀⠈⠛⣶⠋⠀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⠀⡴⢛⡝⠁⠙⠿⡿⠛⠚⢇⠀⠀⠀⣽⠀⠀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⢸⠁⣾⠁⠀⠀⠀⠀⠀⠀⢈⣳⡦⠚⠁⠀⠀⠀⠀⠀⠀\n    ⠀⣴⢟⣿⣦⣄⡀⠈⢧⣿⠀⠀⠀⠀⠀⠀⠀⠸⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀\n    ⠀⠹⣮⣷⣤⡈⠙⠛⠲⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠈⠙⠳⢦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠻⣖⠦⣤⣤⡄⠀⠀⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠋⠀⠸⣿⡗⠀⠀⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠒⠉⠘⠧⡤⠴⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀')
+      except Exception as ex:
+        print(ex)
+
+if __name__ == '__main__':
+  process1 = Process(target=lmaoSnooCockBot)
+  process2 = Process(target=keep_alive)
+  process1.start()
+  process2.start()
+
+  process1.join()
+  process2.join()
